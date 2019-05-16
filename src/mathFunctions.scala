6417db8a4622cbad7407010a2d1d4cba57b10508
rahulmadaan <rahulma@thoughtworks.com>
2019-05-16 17:59:40 +0530

    tried math functions

---
+++ src/mathFunctions.scala
@@ -0,0 +1,9 @@
+object mathFunctions extends  App{
+
+  def add(number1 : Int, number2 : Int) = number1 + number2
+  println("Addition is "+add(2, 4))
+
+  def square(number : Int) = number * number
+  println("square is "+square(2))
+  
+}
